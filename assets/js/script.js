40a0e117eeb0ea743e8da52bf200a43a613fea84
Gabriel Carvalho <gc201319@gmail.com>
2025-11-24 11:13:20 -0300

    fiz a forma de listar e adicionar as tarefas. Falta excluir e fazer o CRUD

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', main)
+
+listaTarefas = [
+    {
+        nomeTarefa: 'Prova MOBILE',
+        descricao: 'Prova de desenvolvimento para aparelhos moveis feita pelo professor leonardo. 4° periodo'
+    }
+]
+function main(){
+    listarTarefas()
+    
+    butao = document.getElementById('butaoCriar')
+    butao.addEventListener('click',() => criarTarefa())
+}
+
+function listarTarefas() {
+    divcorpo = document.getElementById('tarefas')
+    listaTarefas.forEach(tarefa_ => {
+        
+        const divTarefa = document.createElement('div')
+        const tituloTarefa = document.createElement('h2')
+        const descricaoTarefa = document.createElement('p')
+
+        divTarefa.classList.add('tarefa')
+        tituloTarefa.textContent = tarefa_.nomeTarefa
+        descricaoTarefa.textContent = tarefa_.descricao
+
+        divTarefa.appendChild(tituloTarefa)
+        divTarefa.appendChild(descricaoTarefa)
+
+        divcorpo.appendChild(divTarefa)
+    });
+}
+
+function criarTarefa(){
+    nomeTarefa = document.getElementById('tarefaNome')
+    descricaoTarefa = document.getElementById('tarefaDes')
+
+    console.log(nomeTarefa,descricaoTarefa)
+
+}
