2c9cc35ccf1c2d97e9943633fce6e87df6774aa8
Gabriel Carvalho <gc201319@gmail.com>
2025-12-02 10:57:52 -0300

    fiz a ligação com o node/express, agora tem que fazer o banco de dados e os controls tudo e tal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ listaTarefas = [
     {
         nomeTarefa: 'Prova MOBILE',
         descricao: 'Prova de desenvolvimento para aparelhos moveis feita pelo professor leonardo. 4° periodo'
+    },
+    {
+        nomeTarefa: 'Apresentação Arianne',
+        descricao: 'Apresentação de arquitetura orientada a repositorio dia 10/12 '
     }
 ]
 function main(){
@@ -11,6 +15,7 @@ function main(){
     
     butao = document.getElementById('butaoCriar')
     butao.addEventListener('click',() => criarTarefa())
+
 }
 
 function listarTarefas() {
@@ -18,17 +23,34 @@ function listarTarefas() {
     listaTarefas.forEach(tarefa_ => {
         
         const divTarefa = document.createElement('div')
+        const divHeadTarefa = document.createElement('div')
+        const checkboxTarefa = document.createElement('input')
+        checkboxTarefa.type = 'checkbox'
         const tituloTarefa = document.createElement('h2')
         const descricaoTarefa = document.createElement('p')
 
         divTarefa.classList.add('tarefa')
+        divHeadTarefa.classList.add('headTarefa')
         tituloTarefa.textContent = tarefa_.nomeTarefa
         descricaoTarefa.textContent = tarefa_.descricao
 
-        divTarefa.appendChild(tituloTarefa)
+        divHeadTarefa.appendChild(tituloTarefa)
+        divHeadTarefa.appendChild(checkboxTarefa)
+        divTarefa.appendChild(divHeadTarefa)
         divTarefa.appendChild(descricaoTarefa)
 
         divcorpo.appendChild(divTarefa)
+
+        checkboxTarefa.addEventListener('change', () =>{
+        if ( checkboxTarefa.checked){
+            divTarefa.classList.add('concluida')
+        } else {
+            divTarefa.classList.remove('concluida')
+        }
+})
+
+
+
     });
 }
 
@@ -39,4 +61,9 @@ function criarTarefa(){
     console.log(nomeTarefa,descricaoTarefa)
     nomeTarefa.value = ''
     descricaoTarefa.value = ''
+
+
+    const modalEl = document.getElementById('menuCentral');
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
 }
